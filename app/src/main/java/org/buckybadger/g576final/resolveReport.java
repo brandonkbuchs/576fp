2b54aced0d4f3186269602bdf6ee586849ae5e2a
brakebu <brakebu@users.noreply.github.com>
2019-12-09 18:11:34 -0500

    Pass Variables
    Pass variables from viewMap.java to resolveReport.java thru aSyncHttpPost's onQueryReportExecute method.

--- app/src/main/java/org/buckybadger/g576final/resolveReport.java
+++ app/src/main/java/org/buckybadger/g576final/resolveReport.java
@@ -45,13 +45,37 @@ public class resolveReport extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resolve_report);
 
+        //Retrieve variables from viewMap.java, STEP 1
+        Intent mIntent = getIntent();
+
+        Bundle reportDetails = mIntent.getExtras();
+
+        try {
+
+            String reportType = reportDetails.getString("reportType");
+            String reportDesc = reportDetails.getString("reportDesc");
+            String reportID = reportDetails.getString("reportID");
+            Double reportLat = reportDetails.getDouble("reportLat");
+            Double reportLng = reportDetails.getDouble("reportLng");
+
+            Log.v("Report Details", reportType + "\n" + reportDesc + "\n" + reportID + "\n"
+                    + reportLat + "\n" + reportLng);
+        } catch (Exception e) {
+            Log.d("Bundle Error: ", e.toString());
+        }
+
+
+
+
+
+
         /**TODO
-         * 1. Query All Reports in DB
-         * 2. Add Radio Buttons next to each id
-         * 3. Build function to run query on the selected radio button
-         * 3a. Add check to make sure radio button is selected if resolve clicked
-         * 4. Send UPDATE report to database
-         * 5. Return user to map after update, ensure no 'resolved' reports populate
+         *
+         *  1. !!Modify to Just pull the report that was clicked onMarkerClick
+         *
+         * 2. Add function to resolveButton to UPDATE
+         *
+         * 3. Return user to map after update, ensure no 'resolved' reports populate (done)
          * 6. Profit.
          */
 
@@ -60,13 +84,13 @@ public class resolveReport extends AppCompatActivity {
         resolveReport.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {//TODO CODE HERE
+            public void onClick(View view) {//Step 2 of TODO
             }
         });
 
         returnToMap = (Button)findViewById(R.id.goback);
 
-        returnToMap.setOnClickListener( new View.OnClickListener() {
+        returnToMap.setOnClickListener( new View.OnClickListener() { //Step 3 of the TODO
             @Override
             public void onClick(View view) {
                 Intent myIntent = new Intent(resolveReport.this, viewMap.class);
