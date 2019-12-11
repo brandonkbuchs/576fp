9a476d35239cd084500dd3a3cbd0d5e3213d8be1
huntermray <35588874+huntermray@users.noreply.github.com>
2019-12-10 23:57:29 -0600

    init commit
    initial commit of splashscreen code

---
+++ app/src/main/java/org/buckybadger/g576final/SplashActivity.java
@@ -0,0 +1,27 @@
+package org.buckybadger.g576final;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
